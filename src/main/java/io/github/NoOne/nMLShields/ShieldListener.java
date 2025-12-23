ba5b79c58de8e2173ac3665150762e40b15c8453
NoOne_NoBody <hypixelnerd347@gmail.com>
2025-12-23 15:20:28 -0500

    downdated listener

--- src/main/java/io/github/NoOne/nMLShields/ShieldListener.java
+++ src/main/java/io/github/NoOne/nMLShields/ShieldListener.java
@@ -2,8 +2,11 @@ package io.github.NoOne.nMLShields;
 
 import io.github.NoOne.damagePlugin.customDamage.CustomDamageEvent;
 import io.github.NoOne.damagePlugin.customDamage.DamageType;
+import io.github.NoOne.expertiseStylePlugin.abilitySystem.AbilityItemManager;
 import io.github.NoOne.nMLItems.ItemSystem;
 import io.github.NoOne.nMLItems.ItemType;
+import io.github.NoOne.nMLPlayerStats.profileSystem.ProfileManager;
+import io.github.NoOne.nMLPlayerStats.statSystem.Stats;
 import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,13 +14,19 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerGameModeChangeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerItemHeldEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class ShieldListener implements Listener {
     private GuardingSystem guardingSystem;
@@ -25,7 +34,7 @@ public class ShieldListener implements Listener {
     public ShieldListener(NMLShields nmlShields) {
         guardingSystem = nmlShields.getGuardingSystem();
     }
-
+    
     @EventHandler(priority = EventPriority.HIGH)
     public void blockIncomingCustomDamage(CustomDamageEvent event) {
         if (event.getTarget() instanceof Player player && player.isBlocking()) {
@@ -41,16 +50,6 @@ public class ShieldListener implements Listener {
             player.setNoDamageTicks(player.getMaximumNoDamageTicks());
             player.playSound(player.getLocation(), Sound.ITEM_SHIELD_BLOCK, 1f, 1f);
             guardingSystem.damageBar(player, totalDamage);
-        }
-    }
-
-    @EventHandler
-    public void dontUseMainHandShields(PlayerInteractEvent event) {
-        if (ItemSystem.getItemType(event.getPlayer().getInventory().getItemInMainHand()) == ItemType.SHIELD &&
-            (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)) {
-
-            event.setCancelled(true);
-            event.getPlayer().sendMessage("§c⚠ §nShields are offhand exclusive!§r§c ⚠");
         }
     }
 }
